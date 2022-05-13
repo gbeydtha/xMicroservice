97f449f3bea6269e44bcea0f0769ddbbb28d5234
gbeydtha <beydtha@gmail.com>
2022-05-13 03:54:18 +0200

    CQRS implementation started ,, not finish yet

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersDto>>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper)); 
+        }
+
+        public async Task<List<OrdersDto>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var ordersList = await _repository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrdersDto>>(ordersList); 
+        }
+    }
+}
